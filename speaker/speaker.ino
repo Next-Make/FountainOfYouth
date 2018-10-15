6a0fd0764e14aac21d9b3edd16beeff1166bfb4b
Miles Dai <milesdai@mit.edu>
2018-10-14 22:57:17 -0400

    Teensy now generates sine wave on board without usb connection.

--- speaker/speaker.ino
+++ speaker/speaker.ino
@@ -4,35 +4,30 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-AudioInputUSB            usb1;           //xy=200,69  (must set Tools > USB Type to Audio)
+AudioSynthWaveform       waveform1;      //xy=110,75
 AudioOutputI2S           i2s1;           //xy=365,94
-AudioConnection          patchCord1(usb1, 0, i2s1, 0);
-AudioConnection          patchCord2(usb1, 1, i2s1, 1);
+AudioConnection          patchCord1(waveform1, 0, i2s1, 0);
+AudioConnection          patchCord2(waveform1, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=302,184
 
 void setup() {
   AudioMemory(12);
-  sgtl5000_1.enable();
-  sgtl5000_1.volume(0.6);
+//  AudioMemory(15);
+  waveform1.begin(WAVEFORM_SINE);
+  waveform1.frequency(440);
+  waveform1.amplitude(0.3);
 }
 
 void loop() {
-  // read the PC's volume setting
-  float vol = usb1.volume();
-
-  // scale to a nice range (not too loud)
-  // and adjust the audio shield output volume
-  if (vol > 0) {
-    // scale 0 = 1.0 range to:
-    //  0.3 = almost silent
-    //  0.8 = really loud
-    vol = 0.3 + vol * 0.5;
+  for(int i = 220; i < 441; i++){
+    waveform1.frequency(i);
+    delay(50);
   }
-
-  // use the scaled volume setting.  Delete this for fixed volume.
-  sgtl5000_1.volume(vol);
-
-  delay(100);
+  
+  for(int i = 440; i > 219; i--){
+    waveform1.frequency(i);
+    delay(50);
+  }
 }
 
 // Teensyduino 1.35 & earlier had a problem with USB audio on Macintosh
